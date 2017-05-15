832895390ad191d5e56b09aee6c7c77ee8713409
Mousecop <joaskew13@gmail.com>
2017-05-15 11:44:02 -0400

    listens for user Success action adds a new prop to state

--- src/reducers/reducer.jsx
+++ src/reducers/reducer.jsx
@@ -1,11 +1,16 @@
+import * as actions from '../actions/action';
+
 const initalState = {
     messages: [],
-    currentUser: null,
-    onlineUsers: []
+    currentUser: '',
+    Users: [],
+    isUserLoggedIn: false
 }
 
 export default (state = initalState, action) => {
     switch(action.type) {
+        case actions.FETCH_USERS_SUCCESS:
+            return {...state, Users: action.users}
         default:
             return state
     }
