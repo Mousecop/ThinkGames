5dcebd950104273a37b15591d5107e863cf35482
Mousecop <joaskew13@gmail.com>
2017-05-16 17:02:20 -0400

    beginning of routing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 import { Provider } from 'react-redux';
 import store from './store.jsx';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import {userLogin} from './actions/action';
+
+const user = localStorage.getItem('user');
+
+
+if (user) {
+    console.log(user)
+    store.dispatch(userLogin())
+}
 
 ReactDOM.render(
 <Provider store={store}>
