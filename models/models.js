d778f7b9eaddc2243ab6b7fb2c6188fb77c059e7
Mousecop <joaskew13@gmail.com>
2017-05-16 17:02:05 -0400

    removes requirement on email

--- models/models.js
+++ models/models.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt-nodejs');
 const userSchema = mongoose.Schema({
     username: {type: String, require: true, unique: true},
     password: {type: String, require: true},
-    email: {type: String, require: true, unique: true, lowercase: true}
+    email: {type: String, unique: true, lowercase: true}
 });
 
 //Before we save we encrypt the password
