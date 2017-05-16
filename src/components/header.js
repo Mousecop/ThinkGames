6aba0d6c3cd2747a93b58441998058c5c8bdee3d
Mousecop <joaskew13@gmail.com>
2017-05-16 17:03:10 -0400

    header component

---
+++ src/components/header.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router'; //eslint-disable-line no-unused-vars
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props)
+        this.renderLinks = this.renderLinks.bind(this)
+    }
+
+    renderLinks() {
+        if(this.props.isUserLoggedIn) {
+            return [
+                <Link to='/' key={0}>
+                    <li className="brand">ThinkGames</li>
+                </Link>,
+                <Link to='/signout' key={1}>
+                    <li className="signout-button">Sign Out</li>
+                </Link>
+            ]
+        }
+        else {
+            return [
+                <Link to='/' key={0}>
+                    <li className="brand">ThinkGames</li>
+                </Link>,
+                <Link to='/signup' key={1}>
+                    <li className="signup-button">Sign Up</li>
+                </Link>,
+                <Link to='/signin' key={2}>
+                    <li className="signin-button">Sign In</li>
+                </Link>,
+            ]
+        }
+    }
+
+    render() {
+        return(
+            <nav>
+                <ul>
+                    {this.renderLinks()}
+                </ul>
+            </nav>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    isUserLoggedIn : state.isUserLoggedIn
+})
+
+export default connect(mapStateToProps)(Header)
