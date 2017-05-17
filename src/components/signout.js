d0c9a24a6d8e79e9c47f5cc64198c8c9ac1545b2
Mousecop <joaskew13@gmail.com>
2017-05-17 13:10:36 -0400

    fixes name bug for signout action

--- src/components/signout.js
+++ src/components/signout.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions/action';
-import Header from '../header'; //eslint-disable-line no-unused-vars
+import Header from './header'; //eslint-disable-line no-unused-vars
 
 export class Signout extends React.Component {
   componentWillMount() {
-    this.props.signoutUser();
+    this.props.logout();
   }
 
   render() {
