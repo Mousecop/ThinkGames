6acc0b888132be2ae60521c1f76d2389a7231a1e
Mousecop <joaskew13@gmail.com>
2017-05-17 13:08:22 -0400

    Adds routes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 import { Provider } from 'react-redux';
 import store from './store.jsx';
-import { Router, Route, IndexRoute, BrowserHistory } from 'react-router-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import {userLogin} from './actions/action';
+
+import Home from './components/home';
+import Login from './components/login';
+import Signup from './components/signup';
+import Signout from './components/signout';
+import RequireAuth from './components/require_auth';
+import Chat from './components/chat';
+
+
 
 const user = localStorage.getItem('user');
 
@@ -16,7 +25,15 @@ if (user) {
 
 ReactDOM.render(
 <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Home} />
+            <Route path='/login' component={Login}/>
+            <Route path='/signup' component={Signup} />
+            <Route path='/signout' component={Signout} />
+            <Route path='/chat' component={RequireAuth(Chat)} />
+        </Route>
+    </Router>
 </Provider>, 
 document.getElementById('root')
 );
