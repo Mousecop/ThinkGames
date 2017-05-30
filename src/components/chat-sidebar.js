66ad2ea86095ddfc26888139922a3eeb49c01a58
Mousecop <joaskew13@gmail.com>
2017-05-30 19:20:00 -0400

    Starts component for chat window

---
+++ src/components/chat-sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Sidebar extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="chat-sidebar-container">
+                <div className="sidebar-logo">
+                    <img src="" alt="logo" />
+                </div>
+            </div>
+        )
+    }
+}
