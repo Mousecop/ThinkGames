54a9665d569092aa200a94aa428ac0056614ead3
Mousecop <joaskew13@gmail.com>
2017-06-19 20:00:48 -0400

    adds functionality to display previous messages!

--- src/components/chat.js
+++ src/components/chat.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 import Header from './header'; //eslint-disable-line no-unused-vars
 // import SideBar from './chat-sidebar';
 import { connect } from 'react-redux';
+import * as actions from '../actions/action';
 import '../styles/chat.css';
 import '../styles/grid.css';
 
@@ -17,8 +18,10 @@ class Chat extends React.Component {
 
   componentDidMount() {
     this.socket = io('/');
+    this.scrollToBottom();
     this.socket.on('message', (message) => {
       this.setState({messages: [...this.state.messages, message]})
+      this.props.newMessage(message)
     })
     this.socket.on('add user', this.props.currentUser)
   }
@@ -33,6 +36,7 @@ class Chat extends React.Component {
   }
 
   handleSubmit(event) {
+    this.socket.emit('add user', this.props.currentUser)
     const body = event.target.value;
     if(event.keyCode === 13 && body) {
       const message = {
@@ -41,8 +45,9 @@ class Chat extends React.Component {
         createdAt: moment().format('MM/D/YYYY hh:mm:ss')
       }
       this.setState({messages: [...this.state.messages, message]})
-      this.socket.emit('add user', this.props.currentUser)
       this.socket.emit('message', (body))
+      this.props.newMessage(message)
+      console.log('Props user', this.props.currentUser)
       event.target.value = '';
     }
   }
@@ -50,12 +55,24 @@ class Chat extends React.Component {
   render() {
     const fromStyle = {
       color: '#2C8C99',
-      fontSize: '21px'
+      fontSize: '21px',
+      marginRight: '12px'
     }
 
-    const messages = this.state.messages.map((message, index) =>{
-      return (<div>
-        <p key={index} className="message"><b><span style={fromStyle}>{message.from}</span> </b>{message.createdAt}<br/><br/>
+    const messages = this.state.messages.map((message, index)=> {
+      return (<div key={index}>
+        <p className="message"><b><span style={fromStyle}>{message.from}</span> </b>{message.createdAt}<br/><br/>
+        <span className="message-body">{message.body}</span></p><hr/></div>
+      )
+    })
+
+    const sortedMessageHistory = this.props.messages[0].sort((a, b) => {
+      return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+    });
+
+    const messageHistory = sortedMessageHistory.map((message, index) =>{
+      return (<div key={index}>
+        <p className="message"><b><span style={fromStyle}>{message.from}</span> </b>{moment(message.createdAt).format('MM/D/YYYY hh:mm:ss')}<br/><br/>
         <span className="message-body">{message.body}</span></p><hr/></div>
       )
     });
@@ -67,6 +84,7 @@ class Chat extends React.Component {
           <div className="chat-container ">
             <h1 className="chat-title">#general</h1>
             <div className="chat-messages" ref="messages">
+              {messageHistory}
               {messages}
             </div>
             <div className="chat-input">
@@ -82,6 +100,15 @@ class Chat extends React.Component {
 const mapStateToProps = (state) => ({
   currentUser : state.currentUser,
   messages: state.messages
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  newMessage(message) {
+    dispatch(actions.newMessage(message))
+  },
+  messageHistory() {
+    dispatch(actions.fetchMessages())
+    }
 })
 
-export default connect(mapStateToProps)(Chat);
+export default connect(mapStateToProps, mapDispatchToProps)(Chat);
